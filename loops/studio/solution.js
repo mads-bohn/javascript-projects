0c3a8260e2fb5e0a7a0ceeb01c11f73e7f41746c
mads-bohn <mady.bohn@gmail.com>
2024-01-22 19:46:11 -0600

    loops studio part a and b

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,11 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
@@ -15,7 +15,11 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+  for (i = 0; i < numMeals; i++) {
+    let newMeal = [];
+    newMeal.push(protein[i], grains[i], veggies[i], beverages[i], desserts[i]);
+    meals.push(newMeal);
+  }
 
   return meals;
 }
@@ -25,7 +29,9 @@ function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
   
   /// CODE YOUR SOLUTION TO PART B here ///
-
+  while (numMeals < 1 || numMeals > 6 || isNaN(numMeals)) {
+    numMeals = input.question('Please enter a number of meals: ');
+  }
   return numMeals;
 }
 
@@ -45,8 +51,8 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals)
   
 
   /// TEST PART B HERE ///
